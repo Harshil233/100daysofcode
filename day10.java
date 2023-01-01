ac924848e72a79fd0c15aed0a2397f12be1868d0
Harshil Rathod <80687302+Harshil233@users.noreply.github.com>
2023-01-01 19:28:09 +0530

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,46 @@
+/*
+Question:
+
+9. Palindrome Number
+
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
+
+ class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int rev=0; int y=x;
+        while(y>0)
+        {
+            int a=y%10;
+            rev=rev*10+a;
+            y=y/10;
+        }
+        if(rev==x)
+            return true;
+        else 
+            return false;
+        
+    }
+}
