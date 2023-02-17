0cc45aaddd9f35b7643b13762585addd6cb869c6
Harshil Rathod <80687302+Harshil233@users.noreply.github.com>
2023-02-17 23:09:23 +0530

    Update day57.java

--- day57.java
+++ day57.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> getRow(int r) {
         List<Integer> ans = new ArrayList<>();
         ans.add(1);
-        long temp = 1;
+        long temp= 1;
         int up,down=1;
         up=r;
         for(int i=1;i<=r;i++){
