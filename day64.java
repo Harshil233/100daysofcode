f09c963a2f76372315b5e42085b37cec5215e886
Harshil Rathod <80687302+Harshil233@users.noreply.github.com>
2023-02-24 12:47:13 +0530

    Create day64.java

---
+++ day64.java
@@ -0,0 +1,16 @@
+class Solution {
+        public int minSubArrayLen(int target, int[] nums) {
+      int result=Integer.MAX_VALUE;
+      int left=0;
+      int sum=0;
+      for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+         while(sum>=target){
+             result=Math.min(result,i+1-left);
+             sum-=nums[left];
+             left++;
+         }
+      }
+         return result!=Integer.MAX_VALUE? result:0;
+      }
+}
